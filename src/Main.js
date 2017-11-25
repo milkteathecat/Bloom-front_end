0a1f67e48b202c7387abcaa2c89bbc8ae11a398a
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-11-25 15:23:01 -0800

    registration form init

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WrappedRegister } from "./Register";
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <WrappedRegister/>
+            </div>
+        );
+    }
+}
