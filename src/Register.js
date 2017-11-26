b69bf0de04c6b07fd15a5b2deb48b1e8050535f7
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-11-25 16:31:47 -0800

    add demo backend server, regisration talk to backend

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Form, Input, Button, message } from 'antd';
+import $ from 'jquery';
+import { API_ROOT} from "./constants";
 
-import { Form, Input, Button } from 'antd';
 const FormItem = Form.Item;
 
 
@@ -13,6 +15,18 @@ class RegistrationForm extends React.Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/signup`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password
+                    })
+                }).then(function(response) {
+                    message.success(response)
+                }).catch(function(error) {
+                    message.error(error.responseText)
+                });
             }
         });
     }
