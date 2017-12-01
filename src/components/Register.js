0d0f261739d8da44959532cae8f64dccdce8d371
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-11-30 20:21:12 -0800

    jump to login after registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -23,10 +23,13 @@ class RegistrationForm extends React.Component {
                         username: values.username,
                         password: values.password
                     })
-                }).then(function(response) {
-                    message.success(response)
-                }).catch(function(error) {
-                    message.error(error.responseText)
+                }).then((response) => {
+                    message.success(response);
+                    this.props.history.push('/login');
+                },(error) => {
+                    message.error(error.responseText);
+                }).catch((error) => {
+                    message.error(error);
                 });
             }
         });
