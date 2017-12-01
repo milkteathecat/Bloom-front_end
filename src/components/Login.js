5198645cdb3db161bdbb19e0e4c2bf009369be90
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-11-30 20:09:48 -0800

    add jump links

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Form, Icon, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
+
+
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
@@ -33,7 +36,7 @@ class NormalLoginForm extends React.Component {
                     <Button type="primary" htmlType="submit" className="login-form-button">
                         Log in
                     </Button>
-                    Or <a href="">register now!</a>
+                    Or <Link to="/register">register now!</Link>
                 </FormItem>
             </Form>
         );
