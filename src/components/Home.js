bdbe354402527f5b11dc96b3e67a91b5ab10a912
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-03 14:24:54 -0800

    add home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const operations = <Button>Extra Action</Button>;
 export class Home extends React.Component {
     render() {
         return (
-            <Tabs tabBarExtraContent={operations}>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Posts" key="1">
                     Content of tab 1
                 </TabPane>
