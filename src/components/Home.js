377b7bb510efa1dc295e4802659ab631a8d9ce1f
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-03 15:15:32 -0800

    add loading geo location spin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin } from 'antd';
 import { GEO_OPTIONS, POS_KEY } from "../constants";
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    state = {
+        loadingGeoLocation: false,
+    }
+
     componentDidMount() {
         if ("geolocation" in navigator) {
+            this.setState({ loadingGeoLocation: true });
             navigator.geolocation.getCurrentPosition(
                 this.onSuccessLoadGeoLocation,
                 this.onFailedLoadGeoLocation,
@@ -20,19 +25,27 @@ export class Home extends React.Component {
 
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
+        this.setState({ loadingGeoLocation: false });
         const {latitude: lat, longitude: lon} = position.coords;
         localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon: lon}));
     }
 
     onFailedLoadGeoLocation = () => {
+        this.setState({ loadingGeoLocation: false });
+    }
 
+    getGalleryPanelContent = () => {
+        if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading geo location..."/>
+        }
+        return null;
     }
 
     render() {
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Posts" key="1">
-                    Content of tab 1
+                    {this.getGalleryPanelContent()}
                 </TabPane>
                 <TabPane tab="Map" key="2">
                     Content of tab 2
