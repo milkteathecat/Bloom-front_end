7655a5d44a2646b19ee558936441e502dde8643b
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-03 15:05:38 -0800

    save location to localStorage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
-import { GEO_OPTIONS } from "../constants";
+import { GEO_OPTIONS, POS_KEY } from "../constants";
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
@@ -20,6 +20,8 @@ export class Home extends React.Component {
 
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
+        const {latitude: lat, longitude: lon} = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon: lon}));
     }
 
     onFailedLoadGeoLocation = () => {
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const API_ROOT = 'https://around-75015.appspot.com'
 export const TOKEN_KEY = 'TOKEN_KEY'
+export const POS_KEY = 'POS_KEY'
 export const GEO_OPTIONS = {
     enableHighAccuracy: true,
     maximumAge: 3600000,
