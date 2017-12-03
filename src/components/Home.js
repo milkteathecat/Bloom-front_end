3d9a94234f8a0a8e035c442457fc4094833c4f50
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-03 14:14:37 -0800

    add antd tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+const TabPane = Tabs.TabPane;
 
+const operations = <Button>Extra Action</Button>;
 export class Home extends React.Component {
     render() {
         return (
-            <div>
-                This is Home!
-            </div>
+            <Tabs tabBarExtraContent={operations}>
+                <TabPane tab="Posts" key="1">
+                    Content of tab 1
+                </TabPane>
+                <TabPane tab="Map" key="2">
+                    Content of tab 2
+                </TabPane>
+            </Tabs>
         );
     }
 }
