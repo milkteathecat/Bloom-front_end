debeac6c31990e54ac349e61f65128b63120fe55
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-05 21:21:28 -0800

    show self location

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { POS_KEY } from "../constants";
 
 class AroundMap extends React.Component {
     state = {
@@ -11,13 +12,14 @@ class AroundMap extends React.Component {
         });
     }
     render() {
+        const pos = JSON.parse(localStorage.getItem(POS_KEY));
         return (
             <GoogleMap
-                defaultZoom={8}
-                defaultCenter={{ lat: -34.397, lng: 150.644 }}
+                defaultZoom={11}
+                defaultCenter={{ lat: pos.lat, lng: pos.lon }}
             >
                 <Marker
-                    position={{ lat: -34.397, lng: 150.644 }}
+                    position={{ lat: pos.lat, lng: pos.lon }}
                     onClick={this.onToggleOpen}
                 >
                     {this.state.isOpen ? <InfoWindow onCloseClick={this.onToggleOpen}>
