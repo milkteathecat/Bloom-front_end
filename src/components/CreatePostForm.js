1b75f7fc15c997fcc4a2469372c3c7842bdc14a4
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-05 19:58:07 -0800

    add upload dragger

--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Form, Input, Upload, Icon } from 'antd';
 const FormItem = Form.Item;
 
 class CreatePostForm extends React.Component {
+    normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    }
+
     render() {
-        const { form } = this.props;
-        const { getFieldDecorator } = form;
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
         return (
             <Form layout="vertical">
-                <FormItem label="Message">
+                <FormItem
+                    {...formItemLayout}
+                    label="Message">
                     {getFieldDecorator('message', {
-                        rules: [{ required: true, message: 'Please input the title of collection!' }],
+                        rules: [{ required: true, message: 'Please input the message.' }],
                     })(
                         <Input />
                     )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Image"
+                >
+                    <div className="dropbox">
+                        {getFieldDecorator('image', {
+                            rules: [{ required: true, message: 'Please select an image.' }],
+                            valuePropName: 'fileList',
+                            getValueFromEvent: this.normFile,
+                        })(
+                            <Upload.Dragger name="image" action="/upload.do">
+                                <p className="ant-upload-drag-icon">
+                                    <Icon type="inbox" />
+                                </p>
+                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                            </Upload.Dragger>
+                        )}
+                    </div>
                 </FormItem>
             </Form>
         );
