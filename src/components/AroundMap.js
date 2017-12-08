7620d26563903412593051709b9fd1ee52d0ad53
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-07 19:48:03 -0800

    add reload when zoom changes

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -4,19 +4,32 @@ import { POS_KEY } from "../constants";
 import { AroundMarker } from "./AroundMarker";
 
 class AroundMap extends React.Component {
-    onDragEnd = () => {
+    reloadMarkers = () => {
         const center = this.map.getCenter();
         const position = { lat: center.lat(), lon: center.lng() };
-        this.props.loadNearbyPosts(position);
+        this.props.loadNearbyPosts(position, this.getRange());
     }
     getMapRef = (map) => {
         this.map = map;
     }
+
+    getRange = () => {
+        const google = window.google;
+        const center = this.map.getCenter();
+        const bounds = this.map.getBounds();
+        if (center && bounds) {
+            const ne = bounds.getNorthEast();
+            const right = new google.maps.LatLng(center.lat(), ne.lng());
+            return 0.000621371192 * google.maps.geometry.spherical.computeDistanceBetween(center, right);
+        }
+    }
+
     render() {
         const pos = JSON.parse(localStorage.getItem(POS_KEY));
         return (
             <GoogleMap
-                onDragEnd={this.onDragEnd}
+                onDragEnd={this.reloadMarkers}
+                onZoomChanged={this.reloadMarkers}
                 ref={this.getMapRef}
                 defaultZoom={11}
                 defaultCenter={{ lat: pos.lat, lng: pos.lon }}
--- src/components/Home.js
+++ src/components/Home.js
@@ -65,11 +65,12 @@ export class Home extends React.Component {
         return null;
     }
 
-    loadNearbyPosts = (location) => {
-        let { lat, lon } = location ? location : JSON.parse(localStorage.getItem(POS_KEY));
+    loadNearbyPosts = (location, radius) => {
+        const { lat, lon } = location ? location : JSON.parse(localStorage.getItem(POS_KEY));
+        const range = radius ? radius : 200;
         this.setState({ loadingPosts: true });
         return $.ajax({
-           url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=200`,
+           url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=${range}`,
            method: 'GET',
            headers: {
                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
